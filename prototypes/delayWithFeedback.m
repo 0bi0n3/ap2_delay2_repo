1ed18dee635bc601938331957c45d213b3f3cb1d
OberonDW <77157403+0bi0n3@users.noreply.github.com>
2023-07-30 00:10:50 +0100

    added prototypes and audio samples

---
+++ prototypes/delayWithFeedback.m
@@ -0,0 +1,8 @@
+%% Basic feedback delay
+% This script was adapted and referenced from Reiss and McPherson (2015) and Tarr (2019).
+% Please refer to accompanying report reference list for full reference
+% details.
+% Oberon Day-West (21501990).
+%%
+
+
